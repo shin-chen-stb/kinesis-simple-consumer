70a6056d3c2d04f5f1ab037d7d0d0e8847d610de
Shin Chen <shin.chen@stanby.inc>
2021-11-09 18:44:36 +0900

    First commit

---
+++ consumer.py
@@ -0,0 +1,36 @@
+from boto import kinesis
+import time
+
+id='AKIARLZ6R52JFKELZTQN'
+key='LI5sQlJK/F+VGA+zyZJ4QAgA7vZSzviWDpLpyph4'
+auth = {"aws_access_key_id":id, "aws_secret_access_key":key}
+connection = kinesis.connect_to_region('ap-northeast-1',**auth)
+
+
+shard_ids = []
+response = connection.describe_stream('testcdc2')
+stream_name = response['StreamDescription']['StreamName']                   
+print(response)
+for shard_id in response['StreamDescription']['Shards']:
+     shard_id = shard_id['ShardId']
+     print(shard_id)
+     shard_iterator = connection.get_shard_iterator(stream_name, shard_id, 'TRIM_HORIZON')
+     #shard_iterator = connection.get_shard_iterator(stream_name, shard_id, 'LATEST')
+     shard_ids.append({'shard_id' : shard_id ,'shard_iterator' : shard_iterator['ShardIterator'] })
+
+tries = 0
+limit = 100
+result = []
+print(shard_iterator)
+shard_iterator = shard_iterator['ShardIterator']
+while tries < 100:
+     time.sleep(1)
+     tries += 1
+     response = connection.get_records(shard_iterator = shard_iterator)
+     print(response)
+     shard_iterator = response['NextShardIterator']
+     if len(response['Records'])> 0:
+          print(response)
+          for res in response['Records']: 
+               result.append(res)   
+print(result)
